1938e4e3a971773dd29407d021ecbc69b7a9177f
LeeRhodes <lee.rhodes@three.co.uk>
2018-04-27 11:08:53 +0100

    Delete string_changer_spec.rb

--- spec/string_changer_spec.rb
+++
@@ -1,7 +0,0 @@
-class StringChanger
-    def reverse_and_save(string_to_reverse)
-        string_to_reverse.reverse.tap do |reversed_string|
-            File.write('example_file', reversed_string)
-        end
-    end
-end
